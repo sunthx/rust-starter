d03b999acceabc96b1d777759c7e1beceb3dd886
sunthx <sunth0101@126.com>
2021-07-08 18:41:36 +0800

    update guessing_game

--- src/examples/guessing_game/src/main.rs
+++ src/examples/guessing_game/src/main.rs
@@ -39,8 +39,10 @@ fn main() {
             Err(_) => continue,
         };
 
+        let guess = Guess::new(guess);
+
         // match
-        match guess.cmp(&secret_num) {
+        match guess.value().cmp(&secret_num) {
             Ordering::Less => println!("Small"),
             Ordering::Greater => println!("Big"),
             Ordering::Equal => {
@@ -48,5 +50,23 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
     }
 }
